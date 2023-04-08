eb04596a97e45c1f9d2b5358b92ac87b17474962
saifulislam735 <mohimshihab735@gmail.com>
2023-04-08 07:30:38 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+}
+            // tried for font-family set
+//   const defaultTheme = require('tailwindcss/defaultTheme');
+
+// module.exports = {
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         man: ['Manrope',...defaultTheme.fontFamily.sans]
+//       }
+//     }
+//   },
+// }
